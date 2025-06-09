33fd57b0f13b43ad7301ab3951c03d041669c01f
LuisValero21 <ldvalero.18@est.ucab.edu.ve>
2025-06-09 17:29:01 -0400

    Add files via upload

--- node-postgres/src/app.js
+++ node-postgres/src/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const path = require('path');
 const empleadosProfesionalesRoutes = require('./routes/empleadosProfesionalesRoutes');
+const empleadosProfesionalesController = require('./controllers/empleadosProfesionalesController');
+const estructurasRoutes = require('./routes/estructuras');
+const obrasRoutes = require('./routes/obras');
+const artistasRoutes = require('./routes/artistas');
+const coleccionesRoutes = require('./routes/colecciones');
+const eventosRoutes = require('./routes/eventosRoutes');
+const salasExposicionesRoutes = require('./routes/salasExposicionesRoutes');
+const formulariosRoutes = require('./routes/formulariosRoutes');
+const ticketsRoutes = require('./routes/ticketsRoutes');
+const historialMantenimientoRoutes = require('./routes/historialMantenimientoRoutes');
 const dotenv = require("dotenv");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
@@ -15,8 +25,8 @@ const logger = require('./middleware/logger');
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/css', express.static(path.join(__dirname, 'public/css')));
-app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'src/public/css')));
+app.use('/js', express.static(path.join(__dirname, 'src/public/js')));
 app.use('/views', express.static(path.join(__dirname, 'src/views')));
 
 app.set('view engine', 'ejs');
@@ -25,12 +35,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(logger);
 
 // Rutas de los módulos
-app.use('/empleados-profesionales', require('../src/routes/empleadosProfesionalesRoutes'));
-app.use('/estructuras', require('../src/routes/estructurasRoutes'));
-app.use('/obras', require('../src/routes/obrasRoutes'));
-app.use('/eventos', require('../src/routes/eventosRoutes'));
-app.use('/tickets', require('../src/routes/ticketsRoutes'));
-app.use('/historiales-mantenimientos', require('../src/routes/historialMantenimientoRoutes'));
+app.use('/empleados-profesionales', empleadosProfesionalesRoutes);
+app.use('/estructuras', estructurasRoutes);
+app.use('/obras', obrasRoutes);
+app.use('/artistas', artistasRoutes);
+app.use('/colecciones', coleccionesRoutes);
+app.use('/eventos', eventosRoutes);
+app.use('/salas-exposiciones', salasExposicionesRoutes);
+app.use('/formularios', formulariosRoutes);
+app.use('/tickets', ticketsRoutes);
+app.use('/historiales-mantenimientos', historialMantenimientoRoutes);
+app.use('/api/empleados-profesionales', empleadosProfesionalesRoutes);
 
 // Página de inicio
 app.get('/', (req, res) => {
